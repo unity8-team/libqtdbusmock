a0be18ff858a7e97d62a79455b48f69e46c764cf
Robert Bruce Park <robert.park@canonical.com>
2013-08-08 12:40:46 +0200

    Copyright and whitespace cleanup.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2013 Canonical, Ltd.
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3, as published
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU Lesser General Public License as published
  * by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranties of
- * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Author: Pete Woods <pete.woods@canonical.com>
  */
@@ -26,17 +26,17 @@
 using namespace QtDBusMock;
 
 int main(int argc, char **argv) {
-//	qputenv("LANG", "C.UTF-8");
-//	unsetenv("LC_ALL");
+//      qputenv("LANG", "C.UTF-8");
+//      unsetenv("LC_ALL");
 
-//	setlocale(LC_ALL, "");
-//	bindtextdomain(GETTEXT_PACKAGE, LOCALE_DIR);
-//	textdomain(GETTEXT_PACKAGE);
+//      setlocale(LC_ALL, "");
+//      bindtextdomain(GETTEXT_PACKAGE, LOCALE_DIR);
+//      textdomain(GETTEXT_PACKAGE);
 
-	QCoreApplication application(argc, argv);
+        QCoreApplication application(argc, argv);
 
-	DBusMock::registerMetaTypes();
+        DBusMock::registerMetaTypes();
 
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
 }
