6e9445b4f7b767d30a1c4d3d36a1d49f1972f84a
Pete Woods <pete.woods@canonical.com>
2013-08-13 18:42:30 +0100

    More tests, and fix cutom mock bug

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -26,7 +26,7 @@ if (CMAKE_BUILD_TYPE MATCHES coverage)
       message(STATUS "Lcov and genhtml found, can generate HTML coverage info.")
       add_custom_target (coverage-html
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        COMMAND "${LCOV_EXECUTABLE}" --capture --output-file "${CMAKE_BINARY_DIR}/coverage.info" --no-checksum --directory "${CMAKE_BINARY_DIR}"
+        COMMAND "${LCOV_EXECUTABLE}" --capture --output-file "${CMAKE_BINARY_DIR}/coverage.info" --no-checksum --directory "${CMAKE_BINARY_DIR}/src"
         COMMAND "${LCOV_EXECUTABLE}" --remove "${CMAKE_BINARY_DIR}/coverage.info" '/usr/*' --output-file "${CMAKE_BINARY_DIR}/coverage.info" 
         COMMAND "${LCOV_EXECUTABLE}" --remove "${CMAKE_BINARY_DIR}/coverage.info" '${CMAKE_BINARY_DIR}/*' --output-file "${CMAKE_BINARY_DIR}/coverage.info" 
         COMMAND "${LCOV_EXECUTABLE}" --remove "${CMAKE_BINARY_DIR}/coverage.info" '${CMAKE_SOURCE_DIR}/tests/*' --output-file "${CMAKE_BINARY_DIR}/coverage.info" 
--- tests/libqtdbusmock/TestDBusMock.cpp
+++ tests/libqtdbusmock/TestDBusMock.cpp
@@ -18,6 +18,7 @@
 
 #include <libqtdbusmock/DBusMock.h>
 #include <QCoreApplication>
+#include <NetworkManager.h>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -31,57 +32,192 @@ namespace {
 
 class TestDBusMock: public Test {
 protected:
-        TestDBusMock() :
-                        dbusMock(dbusTestRunner) {
-        }
+	TestDBusMock() :
+			dbusMock(dbusTestRunner) {
+	}
 
-        virtual ~TestDBusMock() {
-        }
+	virtual ~TestDBusMock() {
+	}
 
-        bool processListContains(const char *str) {
-                QProcess pgrep;
-                pgrep.start("ps",
-                                QStringList() << "--no-headers" << "--ppid"
-                                                << QString::number(QCoreApplication::applicationPid())
-                                                << "-o" << "args");
-                pgrep.waitForFinished();
-                pgrep.waitForReadyRead();
+	bool processListContains(const char *str) {
+		QProcess pgrep;
+		pgrep.start("ps",
+				QStringList() << "--no-headers" << "--ppid"
+						<< QString::number(QCoreApplication::applicationPid())
+						<< "-o" << "args");
+		pgrep.waitForFinished();
+		pgrep.waitForReadyRead();
 
-                return pgrep.readAll().contains(str);
-        }
+		return pgrep.readAll().contains(str);
+	}
 
-        DBusTestRunner dbusTestRunner;
+	DBusTestRunner dbusTestRunner;
 
-        DBusMock dbusMock;
+	DBusMock dbusMock;
 };
 
 TEST_F(TestDBusMock, StartsDBusMockSession) {
-        dbusMock.registerCustomMock("test.name", "/test/object", "test.Interface",
-                        QDBusConnection::SessionBus);
-        dbusTestRunner.startServices();
+	dbusMock.registerCustomMock("test.name", "/test/object", "test.Interface",
+			QDBusConnection::SessionBus);
+	dbusTestRunner.startServices();
 
-        EXPECT_TRUE(
-                        processListContains(
-                                        "python3 -m dbusmock test.name /test/object test.Interface"));
+	EXPECT_TRUE(
+			processListContains(
+					"python3 -m dbusmock test.name /test/object test.Interface"));
 }
 
 TEST_F(TestDBusMock, StartsDBusMockSystem) {
-        dbusMock.registerCustomMock("test.name2", "/test/object2",
-                        "test.Interface2", QDBusConnection::SystemBus);
-        dbusTestRunner.startServices();
+	dbusMock.registerCustomMock("test.name2", "/test/object2",
+			"test.Interface2", QDBusConnection::SystemBus);
+	dbusTestRunner.startServices();
 
-        EXPECT_TRUE(
-                        processListContains(
-                                        "python3 -m dbusmock -s test.name2 /test/object2 test.Interface2"));
+	EXPECT_TRUE(
+			processListContains(
+					"python3 -m dbusmock -s test.name2 /test/object2 test.Interface2"));
 }
 
 TEST_F(TestDBusMock, StartsDBusMockWithTemplate) {
-        dbusMock.registerNetworkManager();
-        dbusTestRunner.startServices();
+	dbusMock.registerNetworkManager();
+	dbusTestRunner.startServices();
 
-        EXPECT_TRUE(
-                        processListContains(
-                                        "python3 -m dbusmock --template networkmanager"));
+	EXPECT_TRUE(
+			processListContains(
+					"python3 -m dbusmock --template networkmanager"));
+
+	NetworkManagerMockInterface &networkManager(
+			dbusMock.networkManagerInterface());
+	networkManager.AddWiFiDevice("device", "eth1", NM_STATE_DISCONNECTED).waitForFinished();
+
+//	OrgFreedesktopDBusMockInterface &networkManagerMock(
+//			dbusMock.mockInterface(NM_DBUS_SERVICE, NM_DBUS_PATH,
+//					NM_DBUS_INTERFACE, QDBusConnection::SystemBus));
+//	QList<MethodCall> methodCalls(
+//			networkManagerMock.GetMethodCalls("AddWiFiDevice"));
+
+//	ASSERT_EQ(1, methodCalls.size());
+//	ASSERT_EQ(3, methodCalls.first().args().size());
+}
+
+TEST_F(TestDBusMock, GetMethodCalls) {
+	dbusMock.registerCustomMock("test.name", "/test/path", "test.interface",
+			QDBusConnection::SessionBus);
+	dbusTestRunner.startServices();
+
+	OrgFreedesktopDBusMockInterface &mockInterface(
+			dbusMock.mockInterface("test.name", "/test/path", "test.interface",
+					QDBusConnection::SessionBus));
+	mockInterface.AddMethod("test.interface", "MethodName", "s", "s",
+			"ret = 'output'").waitForFinished();
+
+	QDBusMessage message(
+			QDBusMessage::createMethodCall("test.name", "/test/path",
+					"test.interface", "MethodName"));
+	message.setArguments(QVariantList() << QVariant("input"));
+	QDBusMessage reply(dbusTestRunner.sessionConnection().call(message));
+	ASSERT_EQ(1, reply.arguments().size());
+	EXPECT_EQ("output", reply.arguments().first().toString().toStdString());
+
+	QList<MethodCall> methodCalls(mockInterface.GetMethodCalls("MethodName"));
+	ASSERT_EQ(1, methodCalls.size());
+	MethodCall call;
+	call = methodCalls.first();
+	ASSERT_EQ(1, call.args().size());
+	EXPECT_EQ("input", call.args().first().toString().toStdString());
+}
+
+TEST_F(TestDBusMock, GetAllMethodCalls) {
+	dbusMock.registerCustomMock("test.name", "/test/path", "test.interface",
+			QDBusConnection::SessionBus);
+	dbusTestRunner.startServices();
+
+	OrgFreedesktopDBusMockInterface &mockInterface(
+			dbusMock.mockInterface("test.name", "/test/path", "test.interface",
+					QDBusConnection::SessionBus));
+	mockInterface.AddMethod("test.interface", "MethodName", "s", "s",
+			"ret = 'output'").waitForFinished();
+
+	QDBusMessage message(
+			QDBusMessage::createMethodCall("test.name", "/test/path",
+					"test.interface", "MethodName"));
+	message.setArguments(QVariantList() << QVariant("input"));
+	QDBusMessage reply(dbusTestRunner.sessionConnection().call(message));
+	ASSERT_EQ(1, reply.arguments().size());
+	EXPECT_EQ("output", reply.arguments().first().toString().toStdString());
+
+	QList<NamedMethodCall> methodCalls(mockInterface.GetCalls());
+	ASSERT_EQ(1, methodCalls.size());
+	NamedMethodCall call;
+	call = methodCalls.first();
+	EXPECT_EQ("MethodName", call.methodName().toStdString());
+	ASSERT_EQ(1, call.args().size());
+	EXPECT_EQ("input", call.args().first().toString().toStdString());
+}
+
+TEST_F(TestDBusMock, AddMethods) {
+	dbusMock.registerCustomMock("test.name", "/test/path", "test.interface",
+			QDBusConnection::SessionBus);
+	dbusTestRunner.startServices();
+
+	OrgFreedesktopDBusMockInterface &mockInterface(
+			dbusMock.mockInterface("test.name", "/test/path", "test.interface",
+					QDBusConnection::SessionBus));
+
+	Method method1;
+	method1.setName("MethodName1");
+	method1.setInSig("s");
+	method1.setOutSig("s");
+	method1.setCode("ret = 'output1'");
+
+	Method methodTemp;
+	methodTemp.setName("MethodName2");
+	methodTemp.setInSig("u");
+	methodTemp.setOutSig("s");
+	methodTemp.setCode("ret = 'output2'");
+
+	Method method2;
+	method2 = methodTemp;
+
+	mockInterface.AddMethods("test.interface",
+			QList<Method>() << method1 << method2).waitForFinished();
+
+	{
+		QDBusMessage message(
+				QDBusMessage::createMethodCall("test.name", "/test/path",
+						"test.interface", "MethodName1"));
+		message.setArguments(QVariantList() << QVariant("input"));
+		QDBusMessage reply(dbusTestRunner.sessionConnection().call(message));
+		ASSERT_EQ(1, reply.arguments().size());
+		EXPECT_EQ("output1",
+				reply.arguments().first().toString().toStdString());
+	}
+
+	{
+		QDBusMessage message(
+				QDBusMessage::createMethodCall("test.name", "/test/path",
+						"test.interface", "MethodName2"));
+		message.setArguments(QVariantList() << QVariant(123));
+		QDBusMessage reply(dbusTestRunner.sessionConnection().call(message));
+		ASSERT_EQ(1, reply.arguments().size());
+		EXPECT_EQ("output2",
+				reply.arguments().first().toString().toStdString());
+	}
+
+	QList<NamedMethodCall> methodCalls(mockInterface.GetCalls());
+	ASSERT_EQ(2, methodCalls.size());
+	{
+		NamedMethodCall call;
+		call = methodCalls.first();
+		EXPECT_EQ("MethodName1", call.methodName().toStdString());
+		ASSERT_EQ(1, call.args().size());
+		EXPECT_EQ("input", call.args().first().toString().toStdString());
+	}
+	{
+		NamedMethodCall call;
+		call = methodCalls[1];
+		EXPECT_EQ("MethodName2", call.methodName().toStdString());
+		ASSERT_EQ(1, call.args().size());
+		EXPECT_EQ(123, call.args().first().toUInt());
+	}
 }
 
 } // namespace
