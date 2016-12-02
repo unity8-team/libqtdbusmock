0b26850c2eb39f6a3446a59517fb8da7cfe359d2
Pete Woods <pete.woods@canonical.com>
2016-12-02 11:37:50 +0000

    Fix date

--- src/libqtdbusmock/DeclareMetatypes.h
+++ src/libqtdbusmock/DeclareMetatypes.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Canonical, Ltd.
+ * Copyright (C) 2016 Canonical, Ltd.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of version 3 of the GNU Lesser General Public License as published
